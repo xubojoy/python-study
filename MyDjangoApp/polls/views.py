f5b28d05aed4ff23a134aff66f2fed30a4cb0aeb
xubojoy <xubojoy1112@163.com>
2017-08-10 09:54:48 +0800

    添加MyDjangoApp

---
+++ MyDjangoApp/polls/views.py
@@ -0,0 +1,10 @@
+
+# Create your views here.
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def hello(request):
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
